4ca2881109b986b9c5bc53bbf26c1cfcefa9051c
johyep <johonfopili@gmail.com>
2026-09-25 10:59:29 +0000

    com

--- 2zad.py
+++ 2zad.py
@@ -1,1 +1,11 @@
-
+import math
+a, b, h = 0.1, 0.7, 0.05
+print(f"{"x":>6} {"y":>10}")
+print("-"*19)
+x=a
+while x <=b + 1e-9:
+    num=(x**math.sin(x)) + (math.sin(x)**x)
+    den=x**abs(math.cos(2*x)) + math.cos(x)**(2*x)
+    y=num/den
+    print(f"{x:6.2f} {y:10.6f}")
+    x+=h
